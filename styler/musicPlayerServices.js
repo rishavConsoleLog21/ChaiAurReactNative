9eb775b917dda629390c76e00484f01a24c72abd
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-11-02 18:17:38 +0530

    Add Music Player Service

---
+++ styler/musicPlayerServices.js
@@ -0,0 +1,47 @@
+import TrackPlayer, {Event, RepeatMode} from 'react-native-track-player';
+
+import {playListData} from './src/constants';
+
+export async function setupPlayer() {
+  let isSetup = false;
+  try {
+    await TrackPlayer.getActiveTrackIndex();
+    isSetup = true;
+  } catch (error) {
+    await TrackPlayer.setupPlayer();
+    isSetup = true;
+  } finally {
+    return isSetup;
+  }
+}
+
+export async function addTracks() {
+  await TrackPlayer.add(playListData);
+  await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+}
+
+export async function playbackService() {
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.pause();
+  });
+
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.play();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteStop, () => {
+    TrackPlayer.stop();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    TrackPlayer.skipToNext();
+  });
+
+  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+    TrackPlayer.skipToPrevious();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteSeek, () => {
+    TrackPlayer.seekTo();
+  });
+}
