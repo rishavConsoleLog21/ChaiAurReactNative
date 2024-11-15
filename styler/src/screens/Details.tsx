a9aae4201390d0998fa1cef7db048b06ef108f33
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-11-16 00:31:03 +0530

    Add navigation to details page

--- styler/src/screens/Details.tsx
+++ styler/src/screens/Details.tsx
@@ -1,10 +1,29 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const Details = () => {
+// Navigation
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../App';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+
+const Details = ({route}: DetailsProps) => {
+  const {productId} = route.params;
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={styles.container}>
+      <Text style={styles.smallText}>Details: {productId}</Text>
+      <Button
+        title="Go to Home"
+        //onPress={() => navigation.navigate('Home')}
+        //onPress={() => navigation.goBack()} //NOTE: goBack is used to go back to the previous screen
+        //onPress={() => navigation.pop(2)} //NOTE: pop is used to go back to the previous two screens
+        onPress={() => navigation.popToTop()} //NOTE: popToTop is used to go back to the first screen
+      />
     </View>
   );
 };
